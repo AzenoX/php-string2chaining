86f67e0315808ff706631acafb42b16f5a19d490
Alexis Hayat <azen0x.alexis@gmail.com>
2022-04-23 17:44:52 +0200

    feat: add codacy reporter yml file

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # php-string2chaining
 Convert string containing chains into object chains
+
+![Packagist Version](https://img.shields.io/packagist/v/AzenoX/php-string2chaining?label=Version&style=for-the-badge)
+![Packagist Downloads](https://img.shields.io/packagist/dt/AzenoX/php-string2chaining?style=for-the-badge)
+![Packagist License](https://img.shields.io/packagist/l/AzenoX/php-string2chaining?style=for-the-badge)
+![GitHub last commit](https://img.shields.io/github/last-commit/AzenoX/php-string2chaining?style=for-the-badge)
+![Bitbucket open issues](https://img.shields.io/bitbucket/issues/AzenoX/php-string2chaining?style=for-the-badge)
--- src/String2chaining.php
+++ src/String2chaining.php
@@ -9,19 +9,22 @@ class String2chaining
      * @param string $str
      * @return mixed
      */
-    public static function parse($object, string $str)
+    public static function parse(mixed $object, string $str): mixed
     {
         return array_reduce(explode('->', $str), function ($obj, $method) {
-            // If condition is true then it's a property
-            if (strpos($method, '(') === false) {
-                if (!$obj) return null;
-                return $obj->$method;
-            } else { // Else it's a method
-                preg_match('/\((.*?)\)/', $method, $matches);
-                $arguments = explode(',', $matches[1]);
-                $method = explode('(', $method)[0];
-                return $obj->$method(...$arguments);
+            if (!$obj) {
+                return null;
             }
+
+            // If condition is true then it's a property
+            if (!strpos($method, '(')) {
+                return $obj->$method;
+            }
+            // Else it's a method
+            preg_match('/\((.*?)\)/', $method, $matches);
+            $arguments = explode(',', $matches[1]);
+            $method = explode('(', $method)[0];
+            return $obj->$method(...$arguments);
         }, $object);
     }
 }
